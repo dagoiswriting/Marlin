3a0f439298e4811d39be927b5c89f46e5fb9f396
dagoiswriting <dago.gav@gmail.com>
2018-09-02 16:17:47 +0200

    updated Z probe offset
    updated coordinates after moving BLTouch to the back

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -101,10 +101,10 @@
  * Use only one of Left/Right and Front/Behind. Others must be 0 (zero)
  * If you have a dual nozzle the offsets are calculated from the primary nozzle (the one plugged in to E0)
  */
-#define SENSOR_LEFT        22
+#define SENSOR_LEFT        0
 #define SENSOR_RIGHT       0
-#define SENSOR_FRONT       38
-#define SENSOR_BEHIND      0
+#define SENSOR_FRONT       0
+#define SENSOR_BEHIND      73
 
 /**
  * Number of grid points in each direction
